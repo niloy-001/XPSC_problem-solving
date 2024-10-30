70937d37f7e945205695334582f2d0fb7f5afa62
niloy-001 <chowdhuryniloy168@gmail.com>
2024-10-30 23:58:47 +0600

    Add files via upload

---
+++ Day 4/how_maney.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int s,t;
+    cin>>s>>t;
+    int c=0;
+    for(int a=0;a<=s;a++)
+    {
+        
+        for(int b=0;b<=s-a;b++)
+        {
+        for(int c=0;c<=s-a-b;c++)
+        {
+            if(a*b*c<=t)
+            {
+                c++;
+            }
+         }
+
+        }
+    }
+    
+    cout<<c;
+
+    return 0;
+}
