36ba9d2612cbe6b795635b998926e7f7d0780f59
niloy-001 <chowdhuryniloy168@gmail.com>
2024-11-03 22:19:41 +0600

    Add files via upload

---
+++ week2/day 1/C-Plus_or_Minus.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    int sum=a+b;
+
+    if(sum==c) cout<<"+"<<endl;
+    else cout<<"-"<<endl;
+    
+    }
+
+}
