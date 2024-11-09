7f09140f10c9b85703eb78a81afc1abbad324707
niloy-001 <chowdhuryniloy168@gmail.com>
2024-11-09 23:20:28 +0600

    Update B2._The_Strict_Teacher_Hard-Version.cpp

--- week 2/Day 7/B2._The_Strict_Teacher_Hard-Version.cpp
+++ week 2/Day 7/B2._The_Strict_Teacher_Hard-Version.cpp
@@ -1,23 +1,37 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main(){
-
-    long t; scanf("%ld", &t);
-    while(t--){
-        long n, m, q; scanf("%ld %ld %ld", &n, &m, &q);
-        std::vector<long> b(m); for(long p = 0; p < m; p++){scanf("%ld", &b[p]);}
-        sort(b.begin(), b.end());
-        while(q--){
-            long a; scanf("%ld", &a);
+int main() {
+    long t;
+    cin >> t;
+    while (t--) {
+        long n, m, q;
+        scin >> n >> m >> q;
+        
+        std::vector<long> b(m);
+        for (long p = 0; p < m; p++) {
+            cin >> b[p];
+        }
+        
+        std::sort(b.begin(), b.end());
+        
+        while (q--) {
+            long a;
+            cin >> a;
             long pos = std::upper_bound(b.begin(), b.end(), a) - b.begin();
-            long res(0);
-            if(!pos){res = b[0] - 1;}
-            else if(pos == m){res = n - b.back();}
-            else{res = (b[pos] - b[pos - 1]) / 2;}
-            printf("%ld\n", res);
+            long res = 0;
+            
+            if (pos == 0) {
+                res = b[0] - 1;
+            } else if (pos == m) {
+                res = n - b.back();
+            } else {
+                res = (b[pos] - b[pos - 1]) / 2;
+            }
+            
+            cout << res << endl;
         }
     }
 
+    return 0;
 }
