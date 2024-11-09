d8629b18e18f7dbcc34906219194580892d2c197
niloy-001 <chowdhuryniloy168@gmail.com>
2024-11-09 23:22:19 +0600

    Update B.BerlandMusic.cpp

--- week 2/Day 7/B.BerlandMusic.cpp
+++ week 2/Day 7/B.BerlandMusic.cpp
@@ -1,24 +1,26 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main(){
 
-    std::ios_base::sync_with_stdio(false);
+    sios_base::sync_with_stdio(false);
     const long M = 1e7;
 
-    long t; std::cin >> t;
+    long t;
+    cin >> t;
     while(t--){
-        long n; std::cin >> n;
-        std::vector<std::pair<long, long> > v(n);
-        for(long p = 0; p < n; p++){std::cin >> v[p].first; v[p].second = p;}
-        std::string s; std::cin >> s;
+        long n; 
+        cin >> n;
+        vector<pair<long, long> > v(n);
+        for(long p = 0; p < n; p++){cin >> v[p].first; v[p].second = p;}
+        string s; 
+        cin >> s;
         for(long p = 0; p < n; p++){v[p].first += M * (s[p] - '0');}
         sort(v.begin(), v.end());
-        std::vector<long> w(n, 0);
+        vector<long> w(n, 0);
         for(long p = 0; p < n; p++){w[v[p].second] = p + 1;}
-        for(long p = 0; p < n; p++){std::cout << w[p] << " ";}
-        std::cout << std::endl;
+        for(long p = 0; p < n; p++){cout << w[p] << " ";}
+        cout << endl;
     }
 
 }
