cdf2ac74f6c6d2f2354e0ac5571793ee16e56a50
niloy-001 <chowdhuryniloy168@gmail.com>
2024-11-09 23:18:34 +0600

    Update C.Update_Queries.cpp

--- week 2/Day 7/C.Update_Queries.cpp
+++ week 2/Day 7/C.Update_Queries.cpp
@@ -1,31 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main(){
     
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
 
-    long t; std::cin >> t;
+    long t; 
+    cin >> t;
     while(t--){
-        long n, m; std::cin >> n >> m;
-        std::string s; std::cin >> s;
-        std::set<long> t;
+        long n, m; 
+        cin >> n >> m;
+        string s;
+        cin >> s;
+        set<long> t;
         for(long p = 0; p < m; p++){
-            long x; std::cin >> x;
+            long x; 
+            cin >> x;
             t.insert(--x);
         }
-        std::string r; std::cin >> r;
+        string r; 
+        cin >> r;
         sort(r.begin(), r.end());
 
         long idx(0);
-        for(std::set<long>::iterator it = t.begin(); it != t.end(); it++){
+        for(set<long>::iterator it = t.begin(); it != t.end(); it++){
             long pos = *it;
             s[pos] = r[idx++];
         }
 
-        std::cout << s << std::endl;
+        cout << s << endl;
     }
 
 }
