e1a8fa68d4cfe543a87ac1ec345aae3d056b1b4b
niloy-001 <chowdhuryniloy168@gmail.com>
2024-11-09 23:16:43 +0600

    Update C1.PoweringtheHeroeasy_version.cpp

--- week 2/Day 7/C1.PoweringtheHeroeasy_version.cpp
+++ week 2/Day 7/C1.PoweringtheHeroeasy_version.cpp
@@ -1,23 +1,27 @@
-#include <cstdio>
-#include <queue>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long t;
+    cin >> t;
+    while (t--) {
+        long n;
+        cin >> n;
+        priority_queue<long long> ds;
+        long long res = 0;
 
-int main(){
-
-    long t; scanf("%ld", &t);
-    while(t--){
-        long n; scanf("%ld", &n);
-        std::priority_queue<long> ds;
-        long long res(0);
-        for(long p = 0; p < n; p++){
-            long long x; scanf("%lld", &x);
-            if(x){ds.push(x);}
-            else if(!ds.empty()){
+        for (long p = 0; p < n; p++) {
+            long long x;
+            std::cin >> x;
+            if (x) {
+                ds.push(x);
+            } else if (!ds.empty()) {
                 res += ds.top();
                 ds.pop();
             }
         }
 
-        printf("%lld\n", res);
+        cout << res << std::endl;
     }
 
+    return 0;
 }
