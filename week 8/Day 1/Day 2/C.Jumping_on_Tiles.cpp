77614d5e97bcd69d2d72389779db5c12e792b320
niloy-001 <chowdhuryniloy168@gmail.com>
2024-12-20 23:47:13 +0600

    Update C.Jumping_on_Tiles.cpp

--- week 8/Day 1/Day 2/C.Jumping_on_Tiles.cpp
+++ week 8/Day 1/Day 2/C.Jumping_on_Tiles.cpp
@@ -2,50 +2,55 @@
 using namespace std;
 
 int main() {
-    vector<char>s;
-    
-     char c;
+    int t;
+    cin >> t;
 
-    while (cin >> c) {
-        s.push_back(c);
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int n = s.length();
+        char sc = s[0];
+        char ec = s[n - 1];
+
+        int si = sc - 'a' + 1;         // si = start_index
+        int ei = ec - 'a' + 1;          // ei = end_index
+
+        vector<pair<int, int>> inter;
+
+        
+        for (int i = 1; i < n - 1; ++i) {
+            int ci = s[i] - 'a' + 1;
+            if ((si <= ci && ci <= ei) || (ei <= ci && ci <= si)) {
+                inter.emplace_back(ci, i + 1);
+            }
+        }
+
+       
+        if (si <= ei) {
+            sort(inter.begin(), inter.end());
+        } else {
+            sort(inter.rbegin(), inter.rend());
+        }
+
+        
+        vector<int> path = {1}; 
+        for (const auto &p : inter) {
+            path.push_back(p.second);
+        }
+        path.push_back(n); 
+
+       
+        int cost = abs(si - ei);
+        int jumps = path.size();
+
+        
+        cout << cost << " " << jumps << "\n";
+
+        for (int i = 0; i < jumps; ++i) {
+            cout << path[i] << (i + 1 == jumps ? "\n" : " ");
+        }
     }
-
-    int n = s.size();
-
-    map<char, vector<int>> m;
-
-
-    for (int i = 0; i < n; i++) {
-        m[s[i]].push_back(i + 1); 
-    }
-
- 
-    // for (const auto& pair : m) {
-    //     cout << pair.first << ": ";
-    //     for (int index : pair.second) {
-    //         cout << index << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    int cost=abs(s[n-1]-s[0]);
-    vector<int>path;
-
-    // if(s[0]>s[n-1])
-    // {
-    //    for()
-    // }
-
-    // Find the positions of s[0] in the map
-vector<int> positions = m[s[0]];
-
-// Output the positions
-cout << "Positions of '" << s[0] << "' in the input: ";
-for (int pos : positions) {
-    cout << pos << " ";
-}
-cout << endl;
-
 
     return 0;
 }
