30767f936c9c697f2bbb92b94f41d29c3c831b34
niloy-001 <chowdhuryniloy168@gmail.com>
2025-03-23 01:50:13 +0600

    Add files via upload

---
+++ week 3/Day 7/C-Make_My_Array_Equal.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int>a(n);
+        map<int,int>mp;
+
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            mp[a[i]]++;
+        }
+
+        int it=mp.begin()->first;
+
+
+        if(mp.size()==1) cout<<"YES"<<"\n";
+        else if (mp.size()==2 && it==0) cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";
+    }
+
+    return 0;
+}
---
+++ week 3/Day 7/D-Alt-Tab.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    stack<string>s;
+
+    for(int i=0;i<n;i++)
+    {
+        string a;
+        cin>>a;
+        s.push(a);
+    }
+
+    set<string>ans;
+
+    while(!s.empty())
+    {
+
+        if(ans.find(s.top())==ans.end())
+        {
+            string b=s.top();
+            ans.insert(b);
+
+            if(b.size()>2)
+            {
+                cout<<b.substr(b.size()-2);
+            }
+        }
+        s.pop();
+
+    }
+
+    return 0;
+    
+}
